a4893ca7414ca9b4337942ede1209b46d9580d5b
eunseo song <327736953+25730013-a@users.noreply.github.com>
2026-09-17 12:52:33 +0900

    Add files via upload

---
+++ 3주차/3주차_중첩 별찍기.py
@@ -0,0 +1,10 @@
+for y in range(5):
+    for x in range(10):
+        print("*", end="")
+    print("")
+
+#거꾸로 출력
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
